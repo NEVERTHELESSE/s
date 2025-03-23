9dcbb6b2629dcc523b0bfe98e5a2869d6b53b164
NEVERTHELESSE <neverthelesse@localhost.localdomain>
2025-03-23 15:11:11 +0100

    wow

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,7 +3,14 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1>about</h1>
+      <h1>This is the about page from neverthelesse</h1>
+      <img
+        src="/bg2.jpg"
+        style={{
+          width: "40rem",
+        }}
+        alt=""
+      />
     </div>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>home</h1>
+      <h1>This is the home page from neverthelesse</h1>
+      <img
+        src="/bg1.jpg"
+        style={{
+          width: "50rem",
+        }}
+      />
     </div>
   );
 }
